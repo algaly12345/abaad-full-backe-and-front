1ddf672fc5b57ad7e5cf9628c990c9cf07d9dc2e
uygh <ghh>
2026-07-28 10:25:18 +0300

    fix: add missing Agent model import in CustomerAuthController

--- app/Http/Controllers/api/v1/auth/CustomerAuthController.php
+++ app/Http/Controllers/api/v1/auth/CustomerAuthController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Agent;
 
 class CustomerAuthController extends Controller
 {
