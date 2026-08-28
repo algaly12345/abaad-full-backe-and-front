9dbec124b8e358f86bd6ddd5b9ab48f7933f9ac0
uygh <ghh>
2026-08-29 02:14:05 +0300

    fix: register TrustProxies middleware which was defined but never activated, causing Laravel to ignore X-Forwarded-Proto header and generate HTTP URLs behind HTTPS reverse proxy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
